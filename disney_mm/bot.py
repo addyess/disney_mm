5cf2073e9f5d635b5d25540af938613e2c6a32b4
Adam Dyess <adam.dyess@canonical.com>
2021-10-11 12:46:33 -0500

    show specific restaurant menus

--- disney_mm/bot.py
+++ disney_mm/bot.py
@@ -19,27 +19,29 @@ class TouringPlans(object):
     HS = TOURING_PLANS + "/hollywood-studios"
     EPCOT = TOURING_PLANS + "/epcot"
 
-    def _dining_request(self, location):
+    def dining_request(self, location):
         return requests.get(location + self.DINING).json()
 
-    def mk_dining(self):
-        return self._dining_request(self.MK)
+    def all_dining(self):
+        all_req = map(self.dining_request, (self.MK, self.AK, self.HS, self.EPCOT))
+        counter, table = [], []
+        for counter_resp, table_resp in all_req:
+            counter += counter_resp
+            table += table_resp
+        return [counter, table]
 
-    def ak_dining(self):
-        return self._dining_request(self.AK)
-
-    def hs_dining(self):
-        return self._dining_request(self.HS)
-
-    def epcot_dining(self):
-        return self._dining_request(self.EPCOT)
+    def find_dining(self, location):
+        attributes = ['name']
+        loc = location.lower()
+        def match(dine): return any(loc in dine[attr].lower() for attr in attributes)
+        return (list(filter(match, dining)) for dining in self.all_dining())
 
 
 class DisneyPlugin(Plugin):
     def __init__(self):
         super(DisneyPlugin, self).__init__()
         self._settings = bot_settings()
-        self._touring_plans = TouringPlans()
+        self._tp = TouringPlans()
 
     @property
     def mm_settings(self) -> Settings:
@@ -58,23 +60,47 @@ class DisneyPlugin(Plugin):
         human_diff = humanize.precisedelta(diff)
         self.driver.reply_to(message, f"{human_diff}")
 
-    @listen_to("^(?:food|dining|restaurants) (?:in|at) (.*)", needs_mention=True)
+    @listen_to("^(?:[Ff]ood|[Dd]ining|[Rr]estaurants) (?:in|at) (.*)", needs_mention=True)
     async def restaurant(self, message: Message, location: str):
-        req, loc = None, location.lower()
+        resp, loc = None, location.lower()
         if "magic" in loc or "mk" in loc:
-            req = self._touring_plans.mk_dining
+            resp = self._tp.dining_request(self._tp.MK)
         elif "epcot" in loc:
-            req = self._touring_plans.epcot_dining
+            resp = self._tp.dining_request(self._tp.EPCOT)
         elif "animal" in loc or "ak" in loc:
-            req = self._touring_plans.ak_dining
+            resp = self._tp.dining_request(self._tp.AK)
         elif "holly" in loc or "hs" in loc:
-            req = self._touring_plans.hs_dining
+            resp = self._tp.dining_request(self._tp.HS)
 
-        if not req:
-            self.driver.reply_to(message, f"I don't know this park '{location}'?")
+        if not resp:
+            self.driver.reply_to(message, f"I don't know this location '{location}'?")
         else:
-            j = req()
-            names = [spot['name'] for spot in j[0]]  # counter-service
-            self.driver.reply_to(message, f"*Counter Service:* {', '.join(names)}")
-            names = [spot['name'] for spot in j[1]]  # table-service
-            self.driver.reply_to(message, f"*Table Service:* {', '.join(names)}")
+            names = [spot['name'] for spot in resp[0]]  # counter-service
+            if names:
+                self.driver.reply_to(message, f"*Counter Service:* {', '.join(names)}")
+            names = [spot['name'] for spot in resp[1]]  # table-service
+            if names:
+                self.driver.reply_to(message, f"*Table Service:* {', '.join(names)}")
+
+    @listen_to("^(?:[Mm]enu) (?:in|at) (.*)", needs_mention=True)
+    async def menu(self, message: Message, location: str):
+        resp = self._tp.find_dining(location)
+        provided = 0
+        for dining_type in resp:
+            for rest in dining_type:
+                provided += 1
+                self.driver.reply_to(
+                    message,
+                    f"{rest['name']} -- ({rest['category_code']})\n"
+                    f"Selection: {rest['selection']}\n"
+                    f"Menus:\n"
+                    f"   [adult breakfast]({rest['adult_breakfast_menu_url']})\n"
+                    f"   [adult lunch]({rest['adult_lunch_menu_url']})\n"
+                    f"   [adult dinner]({rest['adult_dinner_menu_url']})\n"
+                    f"   [child breakfast]({rest['child_breakfast_menu_url']})\n"
+                    f"   [child lunch]({rest['child_lunch_menu_url']})\n"
+                    f"   [child dinner]({rest['child_dinner_menu_url']})\n"
+                    f"   [disney link]({rest['operator_url']})"
+                )
+        if not provided:
+            self.driver.reply_to(message, f"I couldn't match this location '{location}'?")
